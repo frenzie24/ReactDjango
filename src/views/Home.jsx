52d4e77805233f746dc082dc4cd8745184486dc8
frenzie24 <kevincharlesgross@gmail.com>
2024-07-21 03:50:52 -0400

    added logic to handle whether to direct to landing or login on home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import RoleList from "../components/RoleList";
 import UserList from "../components/UserList";
 import Landing from "./Landing";
 import Login from "./Login";
 
 export default function Home() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+
+
     return (
       <>
-      <Landing />
-       <Login />
+        {loggedIn ? <Landing /> : <Login />}
       </>
     );
   }
